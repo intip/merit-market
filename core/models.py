8caf5fa48b2a3222b2149c81cd52142fd2e77532
Guilherme Vierno <guilhermevierno@gmail.com>
2015-02-06 18:07:14 -0200

    Initial models.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class Customer(AbstractUser):
+
+    hearts = models.PositiveIntegerField(
+        default=0
+    )
+
+    weekly_hearts = models.PositiveSmallIntegerField(
+        default=15
+    )
+
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+
+    def __str__(self):
+        return self.full_name
+
+
+class Transaction(models.Model):
+
+    receiver = models.ForeignKey(
+        related_name=''
+    )
+
+    giver = models.ForeignKey(
+        related_name=''
+    )
+
+    class Meta:
+        verbose_name = "Transaction"
+        verbose_name_plural = "Transactions"
+
+    def __str__(self):
+        pass
+
