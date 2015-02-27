b0a0d0f22a05b5fe8dc19dbef736b92209703703
Guilherme Vierno <guilhermevierno@gmail.com>
2015-02-27 16:55:43 -0300

    Updates README and transaction admin.

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Customer, Transaction
 
 admin.site.register(Customer)
-admin.site.register(Transaction)
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
+
+admin.site.register(Transaction, TransactionAdmin)
