a2fed9c9fb4b7b644c97e0080118d76e63e5a970
Guilherme Vierno <guilhermevierno@gmail.com>
2015-02-27 17:00:53 -0300

    Updates transaction admin with filters.

--- core/admin.py
+++ core/admin.py
@@ -6,5 +6,7 @@ admin.site.register(Customer)
 
 class TransactionAdmin(admin.ModelAdmin):
     model = Transaction
+    list_display = ("giver", "receiver", "qtty", "transaction_time", "comment")
+    list_filter = ("transaction_time", "giver", "receiver")
 
 admin.site.register(Transaction, TransactionAdmin)
